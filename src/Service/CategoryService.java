7ee20e2ee2d9e2dcb01115128c36ae77d6b4b0c5
IT24100566 <it24100566@my.sliit.lk>
2025-09-06 08:23:29 +0530

    Add files via upload

---
+++ src/Service/CategoryService.java
@@ -0,0 +1,30 @@
+package com.vintageonline.service;
+
+import com.vintageonline.model.Category;
+import com.vintageonline.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category update(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
